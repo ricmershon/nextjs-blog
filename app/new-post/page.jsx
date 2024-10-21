b9bee51b4cac8b23bb4c504f46faba269a0ae4ba
Ric Mershon <rmersh@ricmershon.com>
2024-10-21 15:07:20 -0400

    useFormState and action file

--- app/new-post/page.jsx
+++ app/new-post/page.jsx
@@ -1,27 +1,16 @@
-import { redirect } from 'next/navigation';
+'use client';
 
-import { storePost } from '@/lib/actions';
+import { useFormState } from 'react-dom';
+
 import FormSubmit from '@/components/form-submit';
+import { createPost } from '@/lib/actions';
 
 export default function NewPostPage() {
-    async function createPost(formData) {
-        // Required to make this a server action.
-        "use server";
-
-        // Field names defined by `name` vlaues from `input` fields.
-        const title = formData.get('title');
-        const image = formData.get('image');
-        const content = formData.get('content');
-
-        await storePost({ imageUrl: '', title, content, userId: 1 });
-
-        redirect('/feed');
-    }
-
+    const [state, formAction] = useFormState(createPost, {})
     return (
         <>
             <h1>Create a new post</h1>
-            <form action={createPost}>
+            <form action={formAction}>
                 <p className="form-control">
                     <label htmlFor="title">Title</label>
                     <input type="text" id="title" name="title" />
@@ -42,6 +31,13 @@ export default function NewPostPage() {
                 <p className="form-actions">
                     <FormSubmit />
                 </p>
+                {state.errors && (
+                    <ul className="form-errors">
+                        {state.errors.map((error) => (
+                            <li key={error}>{error}</li>
+                        ))}
+                    </ul>
+                )}
             </form>
         </>
     );
